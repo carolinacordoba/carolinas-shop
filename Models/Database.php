58d2a92b1d7c5341c9efe951450548a259a0167a
Carolina Cordoba <carolinacordoba.rg@gmail.com>
2025-03-25 15:26:18 +0100

    started up project

--- Models/Database.php
+++ Models/Database.php
@@ -1,37 +1,42 @@
-<?php 
-    class Database{
-        public $pdo;
+<?php
+class Database
+{
+    public $pdo;
 
-        function __construct() {    
-            $host = "localhost";
-            $db   = "shoppen";
-            $user = "root";
-            $pass = "hejsan123";
+    function __construct()
+    {
+        $host = "localhost";
+        $db = "shoppen";
+        $user = "root";
+        $pass = "root";
 
-            $dsn = "mysql:host=$host;port=3306;dbname=$db";
-            $this->pdo = new PDO($dsn, $user, $pass);
-            $this->initDatabase();
-        }
+        $dsn = "mysql:host=$host:8889;dbname=$db";
+        $this->pdo = new PDO($dsn, $user, $pass);
+        $this->initDatabase();
+    }
 
-        function initDatabase(){
-            $this->pdo->query('CREATE TABLE IF NOT EXISTS Products (
+    function initDatabase()
+    {
+        $this->pdo->query('CREATE TABLE IF NOT EXISTS Products (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 title VARCHAR(50),
                 price INT,
                 stockLevel INT,
                 categoryName VARCHAR(50)
             )');
-        }
-
-        function getAllProducts(){
-            $query = $this->pdo->query('SELECT * FROM Products'); // Products är TABELL 
-            return $query->fetchAll(PDO::FETCH_CLASS, 'Product'); // Product är PHP Klass
-        }
-        function getAllCategories(){
-                // SELECT DISTINCT categoryName FROM Products
-            $data = $this->pdo->query('SELECT DISTINCT categoryName FROM Products')->fetchAll(PDO::FETCH_COLUMN);
-            return $data;
-        }
-
     }
+
+    function getAllProducts()
+    {
+        $query = $this->pdo->query('SELECT * FROM Products'); // Products är TABELL 
+        return $query->fetchAll(PDO::FETCH_CLASS, 'Product'); // Product är PHP Klass
+    }
+    function getAllCategories()
+    {
+        // SELECT DISTINCT categoryName FROM Products
+        $data = $this->pdo->query('SELECT DISTINCT categoryName FROM Products')->fetchAll(PDO::FETCH_COLUMN);
+        return $data;
+    }
+
+}
 ?>
--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $dbContext = new Database();
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Shop Homepage - Start Bootstrap Template</title>
+        <title>LUXÉ BEAUTY</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
@@ -27,12 +27,12 @@ $dbContext = new Database();
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="/index.php">SuperShoppen</a>
+                <a class="navbar-brand" href="/index.php">LUXÉ BEAUTY</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Kategorier</a>
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categories</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="#!">All Products</a></li>
                                 <li><hr class="dropdown-divider" /></li>
@@ -41,7 +41,6 @@ $dbContext = new Database();
                                         echo "<li><a class='dropdown-item' href='#!'>$cat</a></li>";
                                     } 
                                     ?> 
-                                    <li><a class="dropdown-item" href="#!">En cat</a></li>
                             </ul> 
                         </li>
                         <li class="nav-item"><a class="nav-link" href="#!">Login</a></li>
@@ -58,11 +57,11 @@ $dbContext = new Database();
             </div>
         </nav>
         <!-- Header-->
-        <header class="bg-dark py-5">
+        <header class="bg-dark py-5" style="background-image: url('assets/LUXE BEAUTY.webp'); background-size: cover; background-position: center;">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Super shoppen</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">Handla massa onödigt hos oss!</p>
+                    <h1 class="display-4 fw-bolder">LUXÉ BEAUTY</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Where luxury meets effortless beauty</p>
                 </div>
             </div>
         </header>
@@ -79,14 +78,14 @@ $dbContext = new Database();
                                     <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
                                 <?php } ?>        
                                 <!-- Product image-->
-                                <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                                <img class="card-img-top" src="<?php echo $prod->imageUrl; ?>" alt="..." />
                                 <!-- Product details-->
                                 <div class="card-body p-4">
                                     <div class="text-center">
                                         <!-- Product name-->
                                         <h5 class="fw-bolder"><?php echo $prod->title; ?></h5>
                                         <!-- Product price-->
-                                        $<?php echo $prod->price; ?>.00
+                                        <?php echo $prod->price; ?> kr
                                     </div>
                                 </div>
                                 <!-- Product actions-->
@@ -95,37 +94,7 @@ $dbContext = new Database();
                                 </div>
                             </div>
                         </div>    
-                    <?php } ?>  
-                    <div class="col mb-5">
-                        <div class="card h-100">
-                            <!-- Sale badge-->
-                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
-                            <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="...">
-                            <!-- Product details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Product name-->
-                                    <h5 class="fw-bolder">Special Item</h5>
-                                    <!-- Product reviews-->
-                                    <div class="d-flex justify-content-center small text-warning mb-2">
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                    </div>
-                                    <!-- Product price-->
-                                    <span class="text-muted text-decoration-line-through">$20.00</span>
-                                    $18.00
-                                </div>
-                            </div>
-                            <!-- Product actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                            </div>
-                        </div>
-                    </div>         
+                    <?php } ?>          
                 </div>
             </div> 
         </section>
